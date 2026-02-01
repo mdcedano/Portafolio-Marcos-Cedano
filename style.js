1e42f8a91a40c9bf081b31261dad6ef7324805f7
Marcos Cedano <demetrio_marcos@icloud.com>
2026-01-31 23:14:03 -0400

    Version 1.0 Portafolio Marcos Cedano

---
+++ style.js
@@ -0,0 +1,8 @@
+ScrollReveal({
+    origin:"top",
+    distance:'60px',
+    duration: 2500,
+    delay: 400,
+    reset:true
+})
+    ScrollReveal().reveal(`.hero ,.about,.proyectos,.form,.about-title`,{interval:200});
